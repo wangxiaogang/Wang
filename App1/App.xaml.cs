bc3626357ac9d10a51e64c8f9a0493db121bf4eb
minjijuan <minjijuan@live.com>
2013-04-02 07:35:50 +0800

    冲突解决123

--- App1/App.xaml.cs
+++ App1/App.xaml.cs
@@ -27,6 +27,7 @@ namespace App1
     {
         /// <summary>
         /// 小刚测试test
+        /// 冲突test
         /// 初始化单一实例应用程序对象。这是执行的创作代码的第一行，
         /// 逻辑上等同于 main() 或 WinMain()。
         /// VS插件测试 test
