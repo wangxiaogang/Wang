31a2c206dfa7fbcd4e1ddd2a540f9f9d0b11a487
minjijuan <minjijuan@live.com>
2013-04-02 07:14:23 +0800

    小刚

--- App1/App.xaml.cs
+++ App1/App.xaml.cs
@@ -29,6 +29,7 @@ namespace App1
         /// 小刚测试test
         /// 初始化单一实例应用程序对象。这是执行的创作代码的第一行，
         /// 逻辑上等同于 main() 或 WinMain()。
+        /// VS插件测试 test
         /// </summary>
         public App()
         {
